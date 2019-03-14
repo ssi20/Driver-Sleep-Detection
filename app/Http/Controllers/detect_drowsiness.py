ebe3e53cacd8db58b10d7181b505dd32320b8700
Stevert Lobo <stevertlobo@gmail.com>
2019-03-14 21:44:48 +0530

    rechecked detect_drowsiness

--- app/Http/Controllers/detect_drowsiness.py
+++ app/Http/Controllers/detect_drowsiness.py
@@ -116,7 +116,7 @@ while True:
 	else:
 		gamma=1
 
-	frame1 = adjust_gamma(frame,gamma=gamma)
+	frame = adjust_gamma(frame,gamma=gamma)
 	'''frame=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 	frame = clahe.apply(frame)'''
